a357446b32ec70e725090abf0322d567d9bdecec
adanga89 <cuadrado015@gmail.com>
2022-06-17 16:42:46 -0500

    First Endpoint

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola Node");
+const express = require('express');
+//Creador Server Express
+const app = express();
+
+
+//GET
+app.get('/', (reuest, response) => {
+    console.log("Peticion /");
+    response.json({
+        ok: true,
+    })
+})
+
+
+
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en el puerto ${ 4000 }`);
+})
